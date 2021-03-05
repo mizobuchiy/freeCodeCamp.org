827a0b1125fc9ae1dbac9015cb2f5143ebb9d1c4
mizobuchiy <mizobuchi2789@gmail.com>
2021-03-05 18:51:31 +0900

    Database

--- React and Golang JWT Authentication/go-auth/go.mod
+++ React and Golang JWT Authentication/go-auth/go.mod
@@ -2,4 +2,8 @@ module go-auth
 
 go 1.16
 
-require github.com/gofiber/fiber/v2 v2.5.0
+require (
+	github.com/gofiber/fiber/v2 v2.5.0
+	gorm.io/driver/mysql v1.0.4 // indirect
+	gorm.io/gorm v1.21.2 // indirect
+)
--- React and Golang JWT Authentication/go-auth/main.go
+++ React and Golang JWT Authentication/go-auth/main.go
@@ -1,8 +1,26 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"os"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+	"gorm.io/driver/mysql"
+)
 
 func main() {
+	user := os.Getenv("MYSQL_USER")
+  pass := os.Getenv("MYSQL_PASSWORD")
+	host := os.Getenv("MYSQL_HOST")
+  dbname := os.Getenv("MYSQL_DATABASE")
+	connection := fmt.Sprintf("%s:%s@tcp(%s)/%s", user, pass, host, dbname)
+
+	_, err := gorm.Open(mysql.Open(connection), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
