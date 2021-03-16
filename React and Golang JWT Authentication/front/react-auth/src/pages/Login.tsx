8c0e3e71b2416a35a62ab6915e6cd34b9b7f3d3c
mizobuchiy <mizobuchi2789@gmail.com>
2021-03-17 01:00:09 +0900

    Login

--- React and Golang JWT Authentication/front/react-auth/src/pages/Login.tsx
+++ React and Golang JWT Authentication/front/react-auth/src/pages/Login.tsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useState, SyntheticEvent } from "react";
+import { Redirect } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    await fetch("http://localhost:8000/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    setRedirect(true);
+  };
+
+  if (redirect) {
+    return <Redirect to="/" />;
+  }
+
   return (
-    <form>
+    <form onSubmit={submit}>
       <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
       <input
         type="email"
         className="form-control"
         placeholder="Email address"
         required
+        onChange={(e) => setEmail(e.target.value)}
       />
       <input
         type="password"
         className="form-control"
         placeholder="password"
         required
+        onChange={(e) => setPassword(e.target.value)}
       />
 
       <button className="w-100 btn btn-lg btn-primary" type="submit">
