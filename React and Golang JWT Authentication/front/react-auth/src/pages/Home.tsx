726017f0a133fae0b70c8a4a00da1e2b6b2e514c
mizobuchiy <mizobuchi2789@gmail.com>
2021-03-18 02:01:13 +0900

    Authenticated User

--- React and Golang JWT Authentication/front/react-auth/src/pages/Home.tsx
+++ React and Golang JWT Authentication/front/react-auth/src/pages/Home.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("http://localhost:8000/api/user", {
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+      });
+      const content = await response.json();
+
+      setName(content.name);
+    })();
+  });
+
+  return <div>{name ? "Hi " + name : "You are not logged in"}</div>;
 };
 
 export default Home;
