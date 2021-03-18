9bdadbd3924fda487d35d23f45c4f0a666845233
mizobuchiy <mizobuchi2789@gmail.com>
2021-03-19 01:23:45 +0900

    Logout

--- React and Golang JWT Authentication/front/react-auth/src/App.tsx
+++ React and Golang JWT Authentication/front/react-auth/src/App.tsx
@@ -1,19 +1,33 @@
-import React from "react";
-import Login from "./pages/Login";
-import Resister from "./pages/Register";
-import Home from "./pages/Home";
-import Nav from "./components/Nav";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import Login from "./pages/Login";
+import Nav from "./components/Nav";
 import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Resister from "./pages/Register";
 
 function App() {
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("http://localhost:8000/api/user", {
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+      });
+      const content = await response.json();
+
+      setName(content.name);
+    })();
+  });
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Nav />
+        <Nav name={name} />
 
         <main className="form-signin">
-          <Route path="/" exact component={Home} />
+          <Route path="/" exact component={() => <Home name={name} />} />
           <Route path="/login" component={Login} />
           <Route path="/register" component={Resister} />
         </main>
--- React and Golang JWT Authentication/front/react-auth/src/components/Nav.tsx
+++ React and Golang JWT Authentication/front/react-auth/src/components/Nav.tsx
@@ -1,7 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Nav = () => {
+const Nav = (props: { name: string }) => {
+  const logout = async () => {
+    await fetch("http://localhost:8000/api/logout", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+  };
+
+  let menu;
+
+  if (props.name === "" || props.name === undefined) {
+    menu = (
+      <ul className="navbar-nav me-auto mb-2 mb-md-0">
+        <li className="nav-item active">
+          <Link to="/login" className="nav-link">
+            Login
+          </Link>
+        </li>
+        <li className="nav-item active">
+          <Link to="/register" className="nav-link">
+            Register
+          </Link>
+        </li>
+      </ul>
+    );
+  } else {
+    menu = (
+      <ul className="navbar-nav me-auto mb-2 mb-md-0">
+        <li className="nav-item active">
+          <Link to="/login" className="nav-link" onClick={logout}>
+            Logout
+          </Link>
+        </li>
+      </ul>
+    );
+  }
+
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
       <div className="container-fluid">
@@ -9,20 +46,7 @@ const Nav = () => {
           Home
         </Link>
 
-        <div>
-          <ul className="navbar-nav me-auto mb-2 mb-md-0">
-            <li className="nav-item active">
-              <Link to="/login" className="nav-link">
-                Login
-              </Link>
-            </li>
-            <li className="nav-item active">
-              <Link to="/register" className="nav-link">
-                Register
-              </Link>
-            </li>
-          </ul>
-        </div>
+        <div> {menu}</div>
       </div>
     </nav>
   );
