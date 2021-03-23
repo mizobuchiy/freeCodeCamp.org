51ca6699bb7a0bf28815ca231c66241ca4973ccf
mizobuchiy <mizobuchi2789@gmail.com>
2021-03-24 01:35:29 +0900

    Rest APIs with ViewSets

--- Python Microservices Web App/backend/admin/products/serializers.py
+++ Python Microservices Web App/backend/admin/products/serializers.py
@@ -1,8 +1,8 @@
-from rest_framework import serializer
+from rest_framework import serializers
 from .models import Product
 
 
-class ProductSerializer(serializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
