e164dfa780a2724aff07ce06b7dec2fc14c4e9bf
mizobuchiy <mizobuchi2789@gmail.com>
2021-03-28 00:08:29 +0900

    Models

--- Python Microservices Web App/backend/main/main.py
+++ Python Microservices Web App/backend/main/main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from sqlalchemy import UniqueConstraint
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@db/main'
@@ -9,10 +10,18 @@ CORS(app)
 db = SQLAlchemy(app)
 
 
-# class Product(db.model):
-#     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
-#     title = db.Column(db.String(200))
-#     image = db.Column(db.String(200))
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    title = db.Column(db.String(200))
+    image = db.Column(db.String(200))
+
+
+class ProductUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    product_id = db.Column(db.Integer)
+
+    UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
 
 @app.route('/')
